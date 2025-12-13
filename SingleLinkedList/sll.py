f0cad699d20f8daa48f8fecd763d4fd648fb0d80
ilyaskhan15 <ilyaskhanqwer0088@gmail.com>
2025-12-13 23:24:34 +0500

    add push method, insert, pop to the single linked List

--- SingleLinkedList/sll.py
+++ SingleLinkedList/sll.py
@@ -14,16 +14,60 @@ class SingleLinkedList:
             yield current_node
             current_node = current_node.next
         
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node #type: ignore
+            self.tail = new_node
+    
+    def insert(self, position, data):
+        new_node = Node(data)
+        if position == 0:
+            if self.head is None:
+                self.head = new_node
+                self.tail = new_node
+            else:
+                new_node.next = self.head #type: ignore
+                self.head = new_node
+        else:
+            current_node = self.head
+            index = 0
+            while index < position -1:
+                current_node = current_node.next #type: ignore
+                index += 1
+            next_node = current_node.next #type: ignore
+            current_node.next = new_node #type: ignore
+            new_node.next = next_node #type: ignore
+            
+    def pop(self):
+        if self.head is None:
+            print("There are no item to delete")
+            return None
+        else:
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                current_node = self.head
+                while current_node.next != self.tail: #type: ignore
+                    current_node = current_node.next #type: ignore
+                data = self.tail.data #type: ignore
+                self.tail = current_node
+                self.tail.next = None #type: ignore
+                return data
     
     
 sll = SingleLinkedList()
-node1 = Node(12)
-node2 = Node(14)
-node3 = Node(17)
-sll.head = node1 #type: ignore
-node1.next = node2 #type: ignore
-node2.next = node3 #type: ignore
+sll.append(23)
+sll.append(34)
+sll.append(64)
+sll.pop()
+sll.pop()
+sll.pop()
+sll.pop()
 values = [node.data for node in sll]
-
 print(values)
         
